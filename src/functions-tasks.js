909a6f84882e9c84c61ce1323d519d5363b4ed75
asfound <fffound27@gmail.com>
2025-03-20 19:46:28 +0100

    feat: solve logger

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -177,8 +177,17 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const stringifiedArgs = args.map((arg) => JSON.stringify(arg)).join(',');
+    const toLog = `${func.name}(${stringifiedArgs})`;
+
+    logFunc(`${toLog} starts`);
+    const result = func(...args);
+    logFunc(`${toLog} ends`);
+
+    return result;
+  };
 }
 
 /**
